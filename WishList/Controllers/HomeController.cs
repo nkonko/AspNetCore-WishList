03c275a9d5424ecc24af013c55616487ef09b23d
nicolas azzara <nicoazzara@hotmail.com>
2019-07-31 00:32:08 -0300

    Added views

---
+++ WishList/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿namespace WishList.Controller
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Home");
+        }
+
+        public IActionResult Error()
+        {
+            return View("Error");
+        }
+    }
+}
